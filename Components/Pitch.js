46ac854ef82e9cce4f34d763742df9fd49e6504b
Adam <Adamkindel1234@gmail.com>
2022-05-04 17:31:49 +0200

    -

--- Components/Pitch.js
+++ Components/Pitch.js
@@ -11,9 +11,8 @@ function Pitch() {
           Cancelled operation? Long waiting list?
         </h2>
         <h4 className={styles["pitch-text-explain"]}>
-          <h1 className={styles.boldText}>Medopos</h1> can direct you to
-          <h1 className={styles.plainText}> private treatment overseas</h1>, by
-          top physicians without delay and at surprisingly reasonable cost.
+          Medopos can direct you to private treatment overseas, by top
+          physicians without delay and at surprisingly reasonable cost.
         </h4>
       </div>
       <div className={styles["pitch-image-desktop"]}>
